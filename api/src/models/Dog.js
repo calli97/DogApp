0972b2b39fb74c2272e784cb6153d877b6ae343f
calli97 <nahuelcallalli@gmail.com>
2023-03-25 18:55:08 -0300

    init commit

---
+++ api/src/models/Dog.js
@@ -0,0 +1,51 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('dog', {
+    id:{
+      type:DataTypes.INTEGER,
+      primaryKey:true,
+      autoIncrement:true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    weight:{
+      type:DataTypes.STRING,
+      set(value){
+        this.setDataValue('weight',JSON.stringify(value))
+      },
+      get(){
+        const rawValue=this.getDataValue('weight')
+        return JSON.parse(rawValue)
+      },
+      allowNull:false
+    },
+    height:{
+      type:DataTypes.STRING,
+      set(value){
+        this.setDataValue('weight',JSON.stringify(value))
+      },
+      get(){
+        const rawValue=this.getDataValue('weight')
+        return JSON.parse(rawValue)
+      },
+      allowNull:false
+    },
+    life_span:{
+      type:DataTypes.STRING,
+      allowNull:false
+    },
+    image:{
+      type:DataTypes.STRING,
+      allowNull:false,
+      get(){
+        const rawValue=this.getDataValue('image')
+        return `https://cdn2.thedogapi.com/images/${rawValue}.jpg`
+      }
+    }
+  });
+};
