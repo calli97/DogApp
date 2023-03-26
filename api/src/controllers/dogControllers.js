a4e4180a8560fd29e5c81c00df5cbdd5a8f4c1cf
calli97 <nahuelcallalli@gmail.com>
2023-03-26 09:58:56 -0300

    added init readfile

---
+++ api/src/controllers/dogControllers.js
@@ -0,0 +1,40 @@
+const {Dog}=require('../db')
+const fs=require('fs')
+
+const dogControllers={}
+
+dogControllers.getDogs=async(req,res,next)=>{
+    try {
+        const response=await fetch('https://api.thedogapi.com/v1/breeds')
+        const data=await response.json()
+        res.json(data)
+    } catch (error) {
+        console.log(error.message)
+        res.json(error.message)
+    }
+}
+
+dogControllers.getDog=async(req,res,next)=>{
+    const {id}=req.params
+    try {
+        const response=await fetch(`https://api.thedogapi.com/v1/breeds/${id}`)
+        const data=await response.json()
+        res.json(data)
+    } catch (error) {
+        console.log(error.message)
+        res.json(error.message)
+    }
+}
+
+dogControllers.init=async (req,res,next)=>{
+    try {
+        const response=fs.readFileSync('data.json')
+        const data=JSON.parse(response)
+        res.json(data)
+    } catch (error) {
+        console.log(error.message)
+        res.json(error.message)
+    }
+}
+
+module.exports=dogControllers
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,14 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+const dogControllers=require('../controllers/dogControllers')
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
+router.get('/dogs',dogControllers.getDogs)
+router.get('/dogs/:id',dogControllers.getDog)
+router.get('/init',dogControllers.init)
 
 module.exports = router;
