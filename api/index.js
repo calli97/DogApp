d0822521b503218e29a780710ac3cd776474ef90
calli97 <nahuelcallalli@gmail.com>
2023-06-01 21:18:24 -0300

    commit de prueba

--- api/index.js
+++ api/index.js
@@ -20,57 +20,67 @@
 const server = require("./src/app.js");
 const temperamentControllers = require("./src/controllers/temperamentControllers/index.js");
 const { conn } = require("./src/db.js");
-const { Client } = require("pg");
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+// const { Client } = require("pg");
+// const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
-// Syncing all the models at once.
-const client = new Client({
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    port: 5432,
-});
+// // Syncing all the models at once.
+// console.log(DB_USER);
+// console.log(DB_PASSWORD);
+// console.log(DB_HOST);
 
-const createDatabase = async () => {
-    try {
-        await client.connect(); // gets connection
-        //await client.query("CREATE DATABASE dogs_api"); // sends queries
-        ///////Docker user
-        //await client.query("CREATE ROLE postgres WITH LOGIN PASSWORD admin;");
-        /////
-        const checkIfExists = await client.query(
-            "SELECT 1 FROM pg_database WHERE datname = $1",
-            ["dogs_api"]
-        );
-        if (checkIfExists.rowCount > 0) {
-            // Eliminar la base de datos existente
-            await client.query("DROP DATABASE dogs_api");
-        }
-        await client.query("CREATE DATABASE dogs_api");
-        //docker
-        // await client.query("CREATE DATABASE dogs_api WITH OWNER postgres");
+// const client = new Client({
+//     host: DB_HOST,
+//     user: DB_USER,
+//     password: DB_PASSWORD,
+//     port: 5455,
+// });
 
-        return true;
-    } catch (error) {
-        console.error(error.stack);
-        return false;
-    } finally {
-        await client.end(); // closes connection
-    }
-};
+// const createDatabase = async () => {
+//     try {
+//         console.log("INICIA LA CONEXION");
+//         await client.connect(); // gets connection
+//         console.log("CONECTADO");
+//         //await client.query("CREATE DATABASE dogs_api"); // sends queries
+//         ///////Docker user
+//         //await client.query("CREATE ROLE postgres WITH LOGIN PASSWORD admin;");
+//         /////
+//         const checkIfExists = await client.query(
+//             "SELECT 1 FROM pg_database WHERE datname = $1",
+//             ["dogs_api"]
+//         );
+//         console.log("CONECTADO");
+//         if (checkIfExists.rowCount > 0) {
+//             // Eliminar la base de datos existente
+//             await client.query("DROP DATABASE dogs_api");
+//             console.log("ELIMINO SI EXISTE");
+//         }
+//         await client.query("CREATE DATABASE dogs_api");
+//         console.log("CREADAA");
+//         //docker
+//         // await client.query("CREATE DATABASE dogs_api WITH OWNER postgres");
+
+//         return true;
+//     } catch (error) {
+//         console.error(error.stack);
+//         console.log("cacaaaa");
+//         return false;
+//     } finally {
+//         await client.end(); // closes connection
+//     }
+// };
 
 function main() {
-    createDatabase().then((result) => {
-        if (result) {
-            console.log("Database created");
-            conn.sync({ force: true }).then(() => {
-                server.listen(3001, () => {
-                    console.log("%s listening at 3001"); // eslint-disable-line no-console
-                    temperamentControllers.initTemperaments();
-                });
-            });
-        }
+    // createDatabase().then((result) => {
+    //     if (result) {
+    //         console.log("Database created");
+    conn.sync({ force: true }).then(() => {
+        server.listen(3001, () => {
+            console.log("%s listening at 3001"); // eslint-disable-line no-console
+            temperamentControllers.initTemperaments();
+        });
     });
+    //     }
+    // });
 }
 
 main();
