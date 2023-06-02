9adf0d1f38a57335160c1ba7d2aa6eb6788ec192
calli97 <nahuelcallalli@gmail.com>
2023-06-02 15:19:49 -0300

    complet proyect in one docker container

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,42 @@
-import './App.css';
-import {useDispatch} from 'react-redux'
-import {Routes,Route} from 'react-router-dom'
-import Landing from './Pages/Landing/Landing';
-import Home from './Pages/Home/Home'
-import About from './Pages/About/About'
-import Detail from './Pages/Details/Details'
-import Error404 from './Pages/Error/Error404';
-import Navbar from './Components/Navbar/Navbar';
-import { getDogs} from './redux/features/cards/cardsSlice';
-import { useEffect } from 'react';
-import { useRef } from 'react';
-import Form from './Pages/Form/Form';
+import "./App.css";
+import { useDispatch } from "react-redux";
+import { Routes, Route } from "react-router-dom";
+import Landing from "./Pages/Landing/Landing";
+import Home from "./Pages/Home/Home";
+import About from "./Pages/About/About";
+import Detail from "./Pages/Details/Details";
+import Error404 from "./Pages/Error/Error404";
+import Navbar from "./Components/Navbar/Navbar";
+import { getDogs } from "./redux/features/cards/cardsSlice";
+import { useEffect } from "react";
+import { useRef } from "react";
+import Form from "./Pages/Form/Form";
 
 function App() {
-  const initialize=useRef(false)
-  const dispatch=useDispatch()
-  useEffect(() => {
-    
-    if(initialize.current)return
-    dispatch(getDogs())
-    initialize.current=true
-    
-  }, [])
+    const initialize = useRef(false);
+    const dispatch = useDispatch();
+    useEffect(() => {
+        if (initialize.current) return;
+        dispatch(getDogs());
+        initialize.current = true;
+    }, [dispatch]);
 
-  
-  
-  return (
-    <div className="App">
-      <Navbar/>
-      <Routes>
-        <Route path='/' element={<Landing/>}/>
-        <Route path='/home' element={<Home/>}/>
-        <Route path='/about' element={<About/>}></Route>
-        <Route path='/detail/:breedid' element={<Detail></Detail>}></Route>
-        <Route path='/form' element={<Form/>}/>
-        <Route path='*' element={<Error404/>}></Route>
-      </Routes>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Landing />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/about" element={<About />}></Route>
+                <Route
+                    path="/detail/:breedid"
+                    element={<Detail></Detail>}
+                ></Route>
+                <Route path="/form" element={<Form />} />
+                <Route path="*" element={<Error404 />}></Route>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
--- client/src/Pages/Landing/Landing.jsx
+++ client/src/Pages/Landing/Landing.jsx
@@ -1,42 +1,44 @@
-import React, { useEffect } from 'react'
-import styles from './Landing.module.css'
-import { Link } from 'react-router-dom'
+import React from "react";
+import styles from "./Landing.module.css";
+import { Link } from "react-router-dom";
 
 function Landing() {
+    // useEffect(() => {
+    //   fetch('http://localhost:3001/dogs',{
 
-  // useEffect(() => {
-  //   fetch('http://localhost:3001/dogs',{
-      
+    //   })
+    //   .then(response=>response.json())
+    //   .then(data=>console.log(data))
 
-  //   })
-  //   .then(response=>response.json())
-  //   .then(data=>console.log(data))
+    // }, [])
 
-
-  // }, [])
-  
-  return (
-    <div className={styles.page}>
-        <div className={styles.container}>
-          <h1>The Dog App</h1>
-          <div className={styles.options}>
-            <div className={styles.option}>
-              <h4>See the all the breeds avaible!!</h4>
-              <Link to='home' className={`${styles.link} ${styles.buttonLink}`}>
-                Home
-              </Link>
+    return (
+        <div className={styles.page}>
+            <div className={styles.container}>
+                <h1>The Dog App</h1>
+                <div className={styles.options}>
+                    <div className={styles.option}>
+                        <h4>See the all the breeds avaible!!</h4>
+                        <Link
+                            to="home"
+                            className={`${styles.link} ${styles.buttonLink}`}
+                        >
+                            Home
+                        </Link>
+                    </div>
+                    <div className={styles.option}>
+                        <h4> Or create a new one!!</h4>
+                        <Link
+                            to="form"
+                            className={`${styles.link} ${styles.buttonLink}`}
+                        >
+                            Form
+                        </Link>
+                    </div>
+                </div>
             </div>
-            <div className={styles.option}>
-              <h4> Or create a new one!!</h4>
-              <Link to='form' className={`${styles.link} ${styles.buttonLink}`}>
-                Form
-              </Link>
-            </div>
-          </div>
-          
         </div>
-    </div>
-  )
+    );
 }
 
-export default Landing
+export default Landing;
